824ea73a437a292a571d78ae62a5729d1234cc7c
cyprian <cyprian@cyprians-MacBook-Pro.local>
2023-03-13 15:07:01 +0100

    impl reading arguments and parsing html view

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -1,10 +1,43 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const url = require('url');
 
-const app = express()
+const app = express();
+const port = 4080;
 
-app.get('/:videoId/:audioId', (req, res) => {
-    const { videoId, audioId } = req.params
-    res.send(`videoId: ${videoId}, audioId: ${audioId}`)
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(4080)
+
+app.get('/', (req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  res.send(queryObject);
+
+  let playerTag;
+  let playerId;
+  let fileSrc;
+
+  if (queryObject.videoFile) {
+    playerTag = 'video';
+    playerId = 'videoPlayer';
+    fileSrc = queryObject.videoFile;
+  }
+
+  if (queryObject.audioFile) {
+    playerTag = 'audio';
+    playerId = 'audioPlayer';
+    fileSrc = queryObject.audioFile;
+  }
+
+  let html = '';
+  if (playerTag && playerId && fileSrc) {
+    html = `<${playerTag} id="${playerId}" controls><source src="${fileSrc}" type="${playerTag}/mp4"></${playerTag}>`;
+  } else {
+    html = 'Hello World Player';
+  }
+
+  res.send(`<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Hello World Player</title></head><body>${html}</body></html>`);
+});
+
+app.listen(port, () => {
+  console.log(`Hello World Player app listening at http://localhost:${port}`);
+});
